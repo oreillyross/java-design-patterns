f91c6410e4002ac51d4457ec7818fbf3e3df07ff
Ross O'Reilly <oreillyross@gmail.com>
2015-01-30 23:03:16 +0100

    added abstract class Vehicle

---
+++ design patterns/src/strategy/Vehicle.java
@@ -0,0 +1,17 @@
+package strategy;
+
+public abstract class Vehicle {
+  
+	
+	private GoAlgorithm goalgorithm;
+	
+	public Vehicle() {
+		
+	}
+	
+	public void setGoalgorithm(GoAlgorithm goalgorithm) {
+		this.goalgorithm = goalgorithm;
+	}
+  
+  
+}
