ec050056d6322a3c7a1155c79981450fc4507251
Ross O'Reilly <oreillyross@gmail.com>
2015-02-02 07:10:04 -0500

    added SecureOracleConnection file

---
+++ design patterns/src/factory/SecureOracleConnection.java
@@ -0,0 +1,13 @@
+package factory;
+
+public class SecureOracleconnection extends Connection {
+  
+  public SecureOracleconnection() {
+  
+  }	  
+
+  public String description() {
+    return "Oracle Secure!";
+  }
+
+}
