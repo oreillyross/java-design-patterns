eecde8457daa54cc753e8307d586ff37d0e1bde7
Ross O'Reilly <oreillyross@gmail.com>
2015-02-02 23:55:21 +0100

    more comments

--- design patterns/src/observer/Subject.java
+++ design patterns/src/observer/Subject.java
@@ -7,7 +7,7 @@ package observer;
  * @author Ross
  * 
  * This interface lists the methods that subjects like the database system should implement
- *
+ * The Subject objects has to let observers register and has to notify  them when an event occurs.
  */
 public interface Subject {
 	
