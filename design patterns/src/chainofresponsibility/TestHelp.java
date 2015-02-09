43c583c97a87424457401e1a5866b07b45003e5a
Ross O'Reilly <oreillyross@gmail.com>
2015-02-09 01:19:37 +0100

    added TestHelp class

---
+++ design patterns/src/chainofresponsibility/TestHelp.java
@@ -0,0 +1,37 @@
+package chainofresponsibility;
+
+/**
+ * 
+ * @author rreily1978
+ *  
+ *  The Chain of Responsibility design pattern at work. Want to
+ *  handle notifications in a loosely coupled way, but also have a definite chain
+ *  of command in mind? This is the pattern for you.
+ *  
+ */
+
+public class TestHelp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		final int FRONT_END_HELP = 1;
+		final int INTERMEDIATE_LAYER_HELP = 2;
+		final int GENERAL_HELP = 3;
+		
+		Application app = new Application();
+		
+		IntermediateLayer intermediatelayer = new IntermediateLayer(app);
+		
+		FrontEnd frontend = new FrontEnd(intermediatelayer);
+		
+		frontend.getHelp(INTERMEDIATE_LAYER_HELP);
+		frontend.getHelp(GENERAL_HELP);
+		
+		
+
+	}
+
+}
