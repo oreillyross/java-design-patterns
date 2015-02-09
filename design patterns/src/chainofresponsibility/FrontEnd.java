cf00b3e8fc44d1d996f56a4c0f60148fa380fb49
Ross O'Reilly <oreillyross@gmail.com>
2015-02-09 01:03:13 +0100

    updated getHelp method

--- design patterns/src/chainofresponsibility/FrontEnd.java
+++ design patterns/src/chainofresponsibility/FrontEnd.java
@@ -2,7 +2,8 @@ package chainofresponsibility;
 
 public class FrontEnd implements Help {
 
-    Help successor;
+    final int FINAL_FRONT_END_HELP = 1;
+	Help successor;
     
     public FrontEnd(Help h) {
 		successor = h;
@@ -10,7 +11,11 @@ public class FrontEnd implements Help {
 	
 	@Override
 	public void getHelp(int helpConstant) {
-
+      if (helpConstant != FINAL_FRONT_END_HELP) {
+    	  successor.getHelp(helpConstant);
+      } else {
+    	  System.out.println("This is the front end. Don't you like it!");
+      }
 		
 	}
 
