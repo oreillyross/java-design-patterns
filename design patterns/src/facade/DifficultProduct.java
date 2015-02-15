3188f60203986f8ebb7c35de3e6367797f2d061c
Ross O'Reilly <oreillyross@gmail.com>
2015-02-16 00:39:09 +0100

    RobotTemplate completed

--- design patterns/src/facade/DifficultProduct.java
+++ design patterns/src/facade/DifficultProduct.java
@@ -2,42 +2,41 @@ package facade;
 
 public class DifficultProduct {
 
-	
 	char nameChars[] = new char[7];
-	public DifficultProduct()
-	{
+
+	public DifficultProduct() {
 	}
-	public void setFirstNameCharacter(char c)
-	{
-	nameChars[0] = c;
+
+	public void setFirstNameCharacter(char c) {
+		nameChars[0] = c;
 	}
-	public void setSecondNameCharacter(char c)
-	{
-	nameChars[1] = c;
+
+	public void setSecondNameCharacter(char c) {
+		nameChars[1] = c;
 	}
-	public void setThirdNameCharacter(char c)
-	{
-	nameChars[2] = c;
+
+	public void setThirdNameCharacter(char c) {
+		nameChars[2] = c;
 	}
-	public void setFourthNameCharacter(char c)
-	{
-	nameChars[3] = c;
+
+	public void setFourthNameCharacter(char c) {
+		nameChars[3] = c;
 	}
-	public void setFifthNameCharacter(char c)
-	{
-	nameChars[4] = c;
+
+	public void setFifthNameCharacter(char c) {
+		nameChars[4] = c;
 	}
-	public void setSixthNameCharacter(char c)
-	{
-	nameChars[5] = c;
+
+	public void setSixthNameCharacter(char c) {
+		nameChars[5] = c;
 	}
-	public void setSeventhNameCharacter(char c)
-	{
-	nameChars[6] = c;
+
+	public void setSeventhNameCharacter(char c) {
+		nameChars[6] = c;
 	}
-	
+
 	public String getName() {
 		return new String(nameChars);
-		
+
 	}
 }
--- design patterns/src/template/RobotTemplate.java
+++ design patterns/src/template/RobotTemplate.java
@@ -2,6 +2,36 @@ package template;
 
 public abstract class RobotTemplate {
 
-	
-	
+	public final void go() {
+		start();
+		getParts();
+		assemble();
+		test();
+		stop();
+
+	}
+
+	private void stop() {
+		System.out.println("Stopping... ");
+
+	}
+
+	private void test() {
+		System.out.println("Testing.... ");
+	}
+
+	private void assemble() {
+		System.out.println("Assembling... ");
+	}
+
+	private void getParts() {
+		System.out.println("Getting parts... ");
+
+	}
+
+	private void start() {
+		System.out.println("Starting... ");
+
+	}
+
 }
