af04138c7b6255fc84527b709ca9033efa9a8307
Ross O'Reilly <oreillyross@gmail.com>
2015-02-16 00:26:28 +0100

    added - facade pattern complete

---
+++ design patterns/src/facade/TestFacade.java
@@ -0,0 +1,12 @@
+package facade;
+
+public class TestFacade {
+
+	public static void main(String[] args) {
+      SimpleProductFacade sp = new SimpleProductFacade();
+      sp.setName("printer");
+      
+      System.out.println("This product is a : " + sp.getName());
+	}
+
+}
