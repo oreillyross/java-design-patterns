0ef9af79ef020b6f06eefc4ec9951a7a72c8ee1b
Ross O'Reilly <oreillyross@gmail.com>
2015-02-15 20:44:18 +0100

    adapter class added

---
+++ design patterns/src/adapter/AceToAcmeAdapter.java
@@ -0,0 +1,37 @@
+package adapter;
+
+public class AceToAcmeAdapter implements AcmeInterface {
+
+	AceClass aceobject;
+	String firstname;
+	String lastname;
+	
+	public AceToAcmeAdapter(AceClass a) {
+	  aceobject = a;
+	  firstname = aceobject.getName().split(" ")[0];
+      lastname = aceobject.getName().split(" ")[1];
+	}
+	
+	@Override
+	public void setFirstName(String f) {
+		firstname = f
+		
+	}
+
+	@Override
+	public void setLastName(String l) {
+		lastname = l
+		
+	}
+
+	@Override
+	public String getFirstName() {
+		return firstname;
+	}
+
+	@Override
+	public String getLastName() {
+		return lastname;
+	}
+
+}
