f7ad641f7fed2112317580232ba50ac580957011
Ross O'Reilly <oreillyross@gmail.com>
2015-02-16 00:43:38 +0100

    overidding methods from base class

--- design patterns/src/template/AutomotiveRobot.java
+++ design patterns/src/template/AutomotiveRobot.java
@@ -1,5 +1,15 @@
 package template;
 
 public class AutomotiveRobot extends RobotTemplate {
+	public void getParts() {
+		System.out.println("Getting a carburetor....");
+	}
 
+	public void assemble() {
+		System.out.println("Installing the carburetor....");
+	}
+
+	public void test() {
+		System.out.println("Revving the engine....");
+	}
 }
