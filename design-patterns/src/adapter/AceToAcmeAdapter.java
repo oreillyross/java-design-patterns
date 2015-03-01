1c09a5954223acb6e7eb6d563c01332d3e175b98
Ross O'Reilly <oreillyross@gmail.com>
2015-03-01 21:58:07 +0100

    comments

--- design-patterns/src/adapter/AceToAcmeAdapter.java
+++ design-patterns/src/adapter/AceToAcmeAdapter.java
@@ -7,15 +7,22 @@ package adapter;
  * The adapter lets you plug Ace objects (from the user end) into the Acme back end. To adapt Ace to Acme objects 
  * the name is split with the Ace object passed in the constructor.
  * <p>
- * To mimic an Acme object the adaptor implements the {@link AcmeINterface}
+ * To mimic an Acme object the adaptor implements the {@link AcmeInterface}
  *
  */
 public class AceToAcmeAdapter implements AcmeInterface {
 
+	/**
+	 * The adapter needs to store a local copy of the {@link AceClass} object it is going to adapt. 
+	 */
 	AceClass aceobject;
 	String firstname;
 	String lastname;
 	
+	/**
+	 * @param a this is the AceClass passed in and can then adapt its private fields to match the 
+	 * Acme object it is supposed to mimic.
+	 */
 	public AceToAcmeAdapter(AceClass a) {
 	  aceobject = a;
 	  firstname = aceobject.getName().split(" ")[0];
