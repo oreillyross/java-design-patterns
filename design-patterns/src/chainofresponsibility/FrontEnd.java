2aaeba361e8fa399b6f2bd3d02ce1da2454fc33e
Ross O'Reilly <oreillyross@gmail.com>
2015-03-03 22:39:56 +0100

    added javadoc comments for chainofresponsibility pattern

--- design-patterns/src/chainofresponsibility/FrontEnd.java
+++ design-patterns/src/chainofresponsibility/FrontEnd.java
@@ -1,5 +1,9 @@
 package chainofresponsibility;
 
+/**
+ * <h1> These are the layers to handle the requests - this is the starting point </h1>
+ *
+ */
 public class FrontEnd implements Help {
 
     final int FINAL_FRONT_END_HELP = 1;
@@ -9,6 +13,15 @@ public class FrontEnd implements Help {
 		successor = h;
 	}
 	
+	/** 
+	 * The front end can only handle help requests about the front end, which 
+     * uses the constant FRONT_END_HELP. If the constant passed to the FrontEnd
+     * object’s getHelp method is not FRONT_END_HELP, it should pass the help
+     * request to the next object in the chain; otherwise, it knows it can handle
+     * this help request and will display a help message
+	 * 
+	 * @see chainofresponsibility.Help#getHelp(int)
+	 */
 	@Override
 	public void getHelp(int helpConstant) {
       if (helpConstant != FINAL_FRONT_END_HELP) {
