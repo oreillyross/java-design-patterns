d850e0a07ccfd206751ad91bf96a54bd95e8edec
oreillyross <oreillyross@gmail.com>
2015-03-18 07:03:49 -0400

    gradle wrapper

--- design-patterns/src/strategy/Vehicle.java
+++ design-patterns/src/strategy/Vehicle.java
@@ -3,6 +3,10 @@ package strategy;
 public abstract class Vehicle {
   
 	
+	/**
+	 * The abstract Vehicle class stores a private instance of the GoAlgorithm interface (this encapsualtes the behaviour)
+	 * 
+	 */
 	private GoAlgorithm goalgorithm;
 	
 	public Vehicle() {
@@ -11,17 +15,23 @@ public abstract class Vehicle {
 	
 	/**
 	 * when you want to use a particular algorithm in a derived class, all
-     * you’ve got to do is to call the setGoAlgorithm method with the correct
-     * algorithm object
+         * youve got to do is to call the setGoAlgorithm method with the correct
+         * algorithm object
 	 */
+
 	public void setGoalgorithm(GoAlgorithm goalgorithm) {
 		this.goalgorithm = goalgorithm;
 	}
   
-    // calling this method from subclasses means the correct 
-	// algorithm is called despite it being extracted out
-	// of the working part of the subclass or super class Vehicle
+       /** 
+	* Calling this method from subclasses means the correct 
+	* algorithm is called despite it being extracted out
+	* of the working part of the subclass or super class Vehicle
+	* 
+	*/
+
 	public void go() {
-    	goalgorithm.go();
+
+	  goalgorithm.go();
     }
 }
