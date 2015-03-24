8b7d122c5ed46bae94376ed522dc2135ba7014e7
oreillyross <oreillyross@gmail.com>
2015-03-24 15:21:25 -0400

    using ipad to do this

--- design-patterns/src/strategy/GoAlgorithm.java
+++ design-patterns/src/strategy/GoAlgorithm.java
@@ -4,7 +4,7 @@ package strategy;
  * <h1> This interface forms part of the Go behaviour separated out from Vehicle class </h1>
  * <p>
  * The GoAlgorithm interface has one method: go. To make sure any algorithm
-*  can be used by any Vehicle, all algorithms should implement this
+ *  can be used by any Vehicle, all algorithms should implement this
 *  interface, which means they all have to define a go method
  *
  */
