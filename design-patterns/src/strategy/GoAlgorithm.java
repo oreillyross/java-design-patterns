2d27432286269da7b09b765eea09442551549a8d
oreillyross <oreillyross@gmail.com>
2015-03-24 18:23:41 -0400

    minor javdocs update

--- design-patterns/src/strategy/GoAlgorithm.java
+++ design-patterns/src/strategy/GoAlgorithm.java
@@ -5,9 +5,9 @@ package strategy;
  * <p>
  * The GoAlgorithm interface has one method: go. To make sure any algorithm
  *  can be used by any Vehicle, all algorithms should implement this
-*  interface, which means they all have to define a go method
-*  
-*  <p>
+ *  interface, which means they all have to define a go method
+ *
+ * <p>
  *	the part that is going to be changing the most is the go method, so that’s the
  *	part to separate out. In design pattern terms, each implementation of the go
  *	method is called an algorithm (basically that’s just another name for a strategy).
