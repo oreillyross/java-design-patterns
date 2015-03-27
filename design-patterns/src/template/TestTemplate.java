0eea72389c0f2a9b927c4bffda505723d6fea74c
Ross O'Reilly <oreillyross@gmail.com>
2015-03-27 21:46:53 +0100

    javadoc

--- design-patterns/src/template/TestTemplate.java
+++ design-patterns/src/template/TestTemplate.java
@@ -1,5 +1,12 @@
 package template;
 
+
+/**
+ * Template Method will define the skeleton of an algorithm in an operation, deferring some
+ * steps to subclasses. Template Method lets subclasses redefine certain steps
+ * of an algorithm without changing the algorithm’s structure
+ *
+ */
 public class TestTemplate {
 
 	public static void main(String[] args) {
