458c2750e8c5eb35c8868a8f8c68bd2eda67916e
Ross O'Reilly <oreillyross@gmail.com>
2015-04-01 23:03:24 +0200

    javadocs

--- design-patterns/src/template/CookieRobot.java
+++ design-patterns/src/template/CookieRobot.java
@@ -1,5 +1,11 @@
 package template;
 
+/**
+ * <h1> This class also extends the RobotTemplate and defines its own implementation </h1>
+ * The CookieRobot class {@link #getParts()} method displays Getting flour and sugar. . . ., the assemble method displays
+ * Baking a cookie. . . ., and the test method displays Crunching a cookie. . .
+ *
+ */
 public class CookieRobot extends RobotTemplate {
 
 	
